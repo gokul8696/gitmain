a130f25d80ba0a4a41b6fcdc3fbfb8018601b692
gokul8696 <117794376+gokul8696@users.noreply.github.com>
2022-11-09 09:23:57 +0530

    Add files via upload

---
+++ src/test/java/org/test/FridayClass.java
@@ -0,0 +1,17 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class FridayClass {
+	
+	@Test
+	private void test() {
+		System.out.println("plan1");
+		System.out.println("plan2");
+		System.out.println("plan3");
+		System.out.println("plan4");
+		System.out.println("plan5");
+
+	}
+
+}
